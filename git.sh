e4d5a7f9e5dd48790e80ce911dd6e8a12949f7af
Kathinka Gerlinger <36959852+kathinkagerlinger@users.noreply.github.com>
2020-11-16 11:43:09 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@ git config --global user.email "stduierende@mbi-berlin.de"
 git config --global user.name "Studierende"
 git config --global credential.helper cache
 
-git checkout Gruppe6
+git checkout Gruppe7
 git add *
 git commit -m 'neue Simulationsdaten'
 git push
